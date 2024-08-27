8d167d68cce99bc8cb4ad196f510e534535c9c16
Esteve Segura <estevesegura.gir@gmail.com>
2024-08-27 23:25:32 +0200

    refactor: generate keys and iv instead of using hardcoded one

--- examples/model_example.rs
+++ examples/model_example.rs
@@ -1,12 +1,21 @@
 
 use secret_db::db::models::secret_data::{SecretData};
-use secret_db::utils::time::current_timestamp;
+use secret_db::db::models::credential::{Credential};
+use chrono::{Local};
+use rand::Rng;
 
 fn main() {
-    let plaintext_payload = "password: 84717__$ss1Az_2024".to_string();
-    let expiration_date = current_timestamp() + 1000;
+    let iv = (0..16).map(|_| rand::thread_rng().gen_range(0..255)).collect::<Vec<u8>>();
+    let key = (0..16).map(|_| rand::thread_rng().gen_range(0..255)).collect::<Vec<u8>>();
 
-    let secret_data = SecretData::new(&plaintext_payload, expiration_date);
+    let credential = Credential::new(iv,key);
+    println!("{:?}", credential);
+    
+    let plaintext_payload = "password: 84717__$ss1Az_2024".to_string();
+    let current_time:u64 = Local::now().timestamp().try_into().unwrap();
+    let expiration_date = current_time + 1000;
+
+    let secret_data = SecretData::new(&plaintext_payload, expiration_date, &credential);
 
     println!("SecretData: {:?}", secret_data);
 
@@ -14,6 +23,6 @@ fn main() {
     if secret_data.is_expired() {
         println!("Expired secret.");
     } else {
-        println!("Valid secret --- Decrypted: {}", secret_data.decrypt());
+        println!("Valid secret --- Decrypted: {}", secret_data.decrypt(&credential));
     }
 }
