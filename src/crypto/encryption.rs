3c8af3c4e98144cc831eca8f170fe3caa37df0cc
Esteve Segura <estevesegura.gir@gmail.com>
2024-08-28 02:24:56 +0200

    refactor: Add logging to encryption module

--- src/crypto/encryption.rs
+++ src/crypto/encryption.rs
@@ -1,9 +1,11 @@
 use libaes::Cipher;
+use crate::log;
 
 pub fn encrypt(key: &[u8; 16], iv: &[u8; 16], plain_text: &[u8]) -> Vec<u8> {    
     let cipher = Cipher::new_128(key);
     
     let encrypted = cipher.cbc_encrypt(iv, plain_text);
+    log!("INFO", "Encryption successful");
     encrypted
 }
 
@@ -11,5 +13,6 @@ pub fn decrypt(key: &[u8; 16], iv: &[u8; 16], cipher_text: Vec<u8>) -> Vec<u8> {
     let cipher = Cipher::new_128(key);
     
     let decrypted = cipher.cbc_decrypt(iv, &cipher_text);
+    log!("INFO", "Decryption successful");
     decrypted
 }
