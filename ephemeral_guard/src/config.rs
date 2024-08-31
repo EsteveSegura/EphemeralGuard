0d1bbe25ad5b09b2ce5d0d0c98cb57f293d2a4c6
Esteve Segura <estevesegura.gir@gmail.com>
2024-08-31 02:35:20 +0200

    chore: add vars as config

--- ephemeral_guard/src/config.rs
+++ ephemeral_guard/src/config.rs
@@ -4,3 +4,14 @@ pub const HASH_SEED: u32 = 1337;
 // encryption
 pub const ENCRYPTION_KEY: &[u8; 16] = b"This is the key!";
 pub const ENCRYPTION_IV: &[u8; 16] = b"This is 16 bytes";
+
+// server
+pub enum DefaultTypeServer {
+    ZMQ,
+    TCP
+}
+pub const DEFAULT_SERVER: DefaultTypeServer = DefaultTypeServer::ZMQ;
+pub const TCP_SERVER_PORT: &str = "1337";
+
+// cleanup
+pub const CLEANUP_INTERVAL_SECONDS: u64 = 20;
