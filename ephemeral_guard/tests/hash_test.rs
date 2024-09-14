b81d7e9062466af035efbbf5b2e64d8b67069083
Esteve Segura <estevesegura.gir@gmail.com>
2024-09-14 21:50:36 +0200

    style: remove comments

--- ephemeral_guard/tests/hash_test.rs
+++ ephemeral_guard/tests/hash_test.rs
@@ -1,4 +1,4 @@
-extern crate ephemeral_guard; // Nombre de tu crate
+extern crate ephemeral_guard;
 
 use ephemeral_guard::crypto::hash::generate_id;
 
@@ -7,11 +7,9 @@ fn test_generate_id_same_payload_same_seed() {
     let payload = "Hello, World!";
     let seed = 12345;
 
-    // Generar el hash dos veces para el mismo payload y seed
     let hash1 = generate_id(payload, seed);
     let hash2 = generate_id(payload, seed);
 
-    // Verificar que los hashes sean iguales
     assert_eq!(hash1, hash2);
 }
 
@@ -21,11 +19,9 @@ fn test_generate_id_different_payloads() {
     let payload2 = "Goodbye, World!";
     let seed = 12345;
 
-    // Generar hashes para diferentes payloads
     let hash1 = generate_id(payload1, seed);
     let hash2 = generate_id(payload2, seed);
 
-    // Verificar que los hashes sean diferentes
     assert_ne!(hash1, hash2);
 }
 
@@ -35,11 +31,9 @@ fn test_generate_id_different_seeds() {
     let seed1 = 12345;
     let seed2 = 54321;
 
-    // Generar hashes para el mismo payload pero con diferentes seeds
     let hash1 = generate_id(payload, seed1);
     let hash2 = generate_id(payload, seed2);
 
-    // Verificar que los hashes sean diferentes
     assert_ne!(hash1, hash2);
 }
 
@@ -48,9 +42,7 @@ fn test_generate_id_format() {
     let payload = "Hello, World!";
     let seed = 12345;
 
-    // Generar el hash
     let hash = generate_id(payload, seed);
 
-    // Verificar que el hash esté en formato hexadecimal
     assert!(hash.chars().all(|c| c.is_digit(16)));
 }
--- ephemeral_guard/tests/secret_data_test.rs
+++ ephemeral_guard/tests/secret_data_test.rs
@@ -8,31 +8,6 @@ use chrono::Local;
 
 #[test]
 fn test_secret_data_creation() {
-    // Datos de prueba
-    let plaintext = String::from("Sensitive information");
-    let expiration_date = Local::now().timestamp() as u64 + 10000; // Fecha de expiración en el futuro
-    let credential = Credential {
-        encryption_iv: vec![0; 16],
-        encryption_key: vec![1; 16],
-    };
-
-    // Crear instancia de SecretData
-    let secret_data = SecretData::new(&plaintext, expiration_date, &credential);
-
-    // Verificar que el ID fue generado correctamente (hashear el texto encriptado)
-    let encrypted_payload_str = String::from_utf8_lossy(&secret_data.payload).to_string();
-    let expected_id = hash::generate_id(&encrypted_payload_str, HASH_SEED);
-    assert_eq!(secret_data.id, expected_id);
-
-    // Verificar que el payload fue encriptado
-    assert_ne!(secret_data.payload, plaintext.as_bytes().to_vec());
-
-    // Verificar que la fecha de expiración es la correcta
-    assert_eq!(secret_data.expiration_date, expiration_date);
-}
-#[test]
-fn test_secret_data_decryption() {
-    // Datos de prueba
     let plaintext = String::from("Sensitive information");
     let expiration_date = Local::now().timestamp() as u64 + 10000;
     let credential = Credential {
@@ -40,11 +15,28 @@ fn test_secret_data_decryption() {
         encryption_key: vec![1; 16],
     };
 
-    // Crear instancia de SecretData y luego desencriptar
+    let secret_data = SecretData::new(&plaintext, expiration_date, &credential);
+
+    let encrypted_payload_str = String::from_utf8_lossy(&secret_data.payload).to_string();
+    let expected_id = hash::generate_id(&encrypted_payload_str, HASH_SEED);
+    assert_eq!(secret_data.id, expected_id);
+
+    assert_ne!(secret_data.payload, plaintext.as_bytes().to_vec());
+
+    assert_eq!(secret_data.expiration_date, expiration_date);
+}
+#[test]
+fn test_secret_data_decryption() {
+    let plaintext = String::from("Sensitive information");
+    let expiration_date = Local::now().timestamp() as u64 + 10000;
+    let credential = Credential {
+        encryption_iv: vec![0; 16],
+        encryption_key: vec![1; 16],
+    };
+
     let secret_data = SecretData::new(&plaintext, expiration_date, &credential);
     let decrypted_text = secret_data.decrypt(&credential);
 
-    // Verificar que el texto desencriptado es igual al texto plano original
     assert_eq!(decrypted_text, plaintext);
 }
 
@@ -52,8 +44,7 @@ fn test_secret_data_decryption() {
 fn test_secret_data_is_expired() {
     let plaintext = String::from("Sensitive information");
 
-    // Simular una fecha de expiración pasada
-    let expired_date = Local::now().timestamp() as u64 - 10000; // Fecha en el pasado
+    let expired_date = Local::now().timestamp() as u64 - 10000;
     let credential = Credential {
         encryption_iv: vec![0; 16],
         encryption_key: vec![1; 16],
@@ -61,14 +52,11 @@ fn test_secret_data_is_expired() {
 
     let secret_data = SecretData::new(&plaintext, expired_date, &credential);
 
-    // Verificar que el dato ha expirado
     assert!(secret_data.is_expired());
 
-    // Simular una fecha de expiración en el futuro
-    let future_date = Local::now().timestamp() as u64 + 10000; // Fecha en el futuro
+    let future_date = Local::now().timestamp() as u64 + 10000;
     let secret_data_future = SecretData::new(&plaintext, future_date, &credential);
 
-    // Verificar que el dato no ha expirado
     assert!(!secret_data_future.is_expired());
 }
 
@@ -84,7 +72,6 @@ fn test_secret_data_display() {
     let secret_data = SecretData::new(&plaintext, expiration_date, &credential);
     let display_string = format!("{}", secret_data);
 
-    // Verificar el formato de salida de Display
     assert!(display_string.contains(&secret_data.id));
     assert!(display_string.contains(&expiration_date.to_string()));
 }
