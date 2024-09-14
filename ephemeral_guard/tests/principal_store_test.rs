ac20efacc4198a6e4f1f269adfe656c04e477609
Esteve Segura <estevesegura.gir@gmail.com>
2024-09-14 21:49:27 +0200

    styke: remove comments

--- ephemeral_guard/tests/principal_store_test.rs
+++ ephemeral_guard/tests/principal_store_test.rs
@@ -1,4 +1,4 @@
-extern crate ephemeral_guard; // Nombre de tu crate
+extern crate ephemeral_guard;
 
 use ephemeral_guard::db::models::secret_data::SecretData;
 use ephemeral_guard::db::storage::principal_store::PrincipalStore;
@@ -7,11 +7,9 @@ use chrono::Local;
 
 #[test]
 fn test_create_principal_store() {
-    // Crear un PrincipalStore en modo memoria (sin FileStorage)
     let store = PrincipalStore::new(PrincipalStoreMode::InMemory);
 
-    // Verificar que el store y la lista de expiraciones están vacíos
-    assert_eq!(store.get_total_secrets(), 0);  // Usar el nuevo método
+    assert_eq!(store.get_total_secrets(), 0);
     assert_eq!(store.get_total_expirations(), 0);
 }
 
@@ -19,17 +17,14 @@ fn test_create_principal_store() {
 fn test_add_and_get_secret() {
     let mut store = PrincipalStore::new(PrincipalStoreMode::InMemory);
 
-    // Crear un SecretData de prueba
     let secret = SecretData {
         id: String::from("secret_id"),
         payload: b"payload".to_vec(),
         expiration_date: Local::now().timestamp() as u64 + 10000,
     };
 
-    // Agregar el secreto
     store.add_secret(secret.clone());
 
-    // Verificar que el secreto se puede recuperar
     let retrieved_secret = store.get_secret(&String::from("secret_id"));
     assert!(retrieved_secret.is_some());
     assert_eq!(retrieved_secret.unwrap().id, "secret_id");
@@ -39,21 +34,17 @@ fn test_add_and_get_secret() {
 fn test_remove_secret() {
     let mut store = PrincipalStore::new(PrincipalStoreMode::InMemory);
 
-    // Crear un SecretData de prueba
     let secret = SecretData {
         id: String::from("secret_id"),
         payload: b"payload".to_vec(),
         expiration_date: Local::now().timestamp() as u64 + 10000,
     };
 
-    // Agregar el secreto
     store.add_secret(secret.clone());
 
-    // Eliminar el secreto
     let removed = store.remove_secret(&String::from("secret_id"));
     assert!(removed);
 
-    // Verificar que el secreto ya no existe
     let retrieved_secret = store.get_secret(&String::from("secret_id"));
     assert!(retrieved_secret.is_none());
 }
@@ -62,61 +53,49 @@ fn test_remove_secret() {
 fn test_clean_expired_secrets() {
     let mut store = PrincipalStore::new(PrincipalStoreMode::InMemory);
 
-    // Crear un SecretData expirado
     let expired_secret = SecretData {
         id: String::from("expired_secret"),
         payload: b"expired_payload".to_vec(),
-        expiration_date: Local::now().timestamp() as u64 - 1000, // Ya expirado
+        expiration_date: Local::now().timestamp() as u64 - 1000,
     };
 
-    // Crear un SecretData válido
     let valid_secret = SecretData {
         id: String::from("valid_secret"),
         payload: b"valid_payload".to_vec(),
         expiration_date: Local::now().timestamp() as u64 + 10000,
     };
 
-    // Agregar ambos secretos
     store.add_secret(expired_secret.clone());
     store.add_secret(valid_secret.clone());
 
-    // Limpiar los secretos expirados
     store.clean_expired_secrets(10, 1);
 
-    // Verificar que el secreto expirado ha sido eliminado
     let expired_retrieved = store.get_secret(&String::from("expired_secret"));
     assert!(expired_retrieved.is_none());
 
-    // Verificar que el secreto válido sigue existiendo
     let valid_retrieved = store.get_secret(&String::from("valid_secret"));
     assert!(valid_retrieved.is_some());
 }
 
 #[test]
 fn test_save_and_load_file_storage() {
-    let test_file_path = "/tmp/test_principal_store.bin"; // Ruta de archivo temporal para pruebas
+    let test_file_path = "/tmp/test_principal_store.bin";
 
-    // Crear un PrincipalStore en modo FileStorage
     let mut store = PrincipalStore::new(PrincipalStoreMode::FileStorage(String::from(test_file_path)));
 
-    // Crear un SecretData de prueba
     let secret = SecretData {
         id: String::from("file_secret"),
         payload: b"file_payload".to_vec(),
         expiration_date: Local::now().timestamp() as u64 + 10000,
     };
 
-    // Agregar el secreto y guardarlo en archivo
     store.add_secret(secret.clone());
 
-    // Cargar el store desde el archivo
     let new_store = PrincipalStore::new(PrincipalStoreMode::FileStorage(String::from(test_file_path)));
 
-    // Verificar que el secreto ha sido cargado correctamente
     let retrieved_secret = new_store.get_secret(&String::from("file_secret"));
     assert!(retrieved_secret.is_some());
     assert_eq!(retrieved_secret.unwrap().id, "file_secret");
 
-    // Eliminar el archivo de prueba
     std::fs::remove_file(test_file_path).expect("Failed to delete test file");
 }
