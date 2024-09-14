ba8a332172a8f897b3b810badc19557ebe4e150d
Esteve Segura <estevesegura.gir@gmail.com>
2024-09-14 22:38:57 +0200

    refactor: remove unused config

--- ephemeral_guard/src/config.rs
+++ ephemeral_guard/src/config.rs
@@ -3,10 +3,6 @@ use serde::{Serialize, Deserialize};
 // hash
 pub const HASH_SEED: u32 = 1337;
 
-// encryption
-pub const ENCRYPTION_KEY: &[u8; 16] = b"This is the key!";
-pub const ENCRYPTION_IV: &[u8; 16] = b"This is 16 bytes";
-
 // server
 pub enum DefaultTypeServer {
     TCP
